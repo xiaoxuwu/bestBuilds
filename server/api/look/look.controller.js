b58439f5040b921e41c10d4767e0f828d7efa7e1
Xiaoxu (Carter) Wu <carter.wu@ucla.edu>
2016-09-12 10:52:43 -0700

    deployment changes

--- server/api/look/look.controller.js
+++ server/api/look/look.controller.js
@@ -4,7 +4,6 @@ var _ = require('lodash');
 var Look = require('./look.model');
 var path = require('path');
 var express = require('express');
-var utils = require('../../utils/utils.js');
 
 exports.allLooks = function(req, res) {
   Look.find({})
@@ -45,14 +44,9 @@ exports.userLooks = function(req, res) {
 };
 
 exports.scrapeUpload = function(req, res) {
-  var random = utils.randomizer(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-
-  utils.downloadURI(req.body.image, '../client/assets/images/uploads/' + random + '.png', function(filename) {
-    console.log('done');
-
     var newLook = new Look();
     newLook.title = req.body.title;
-    newLook.image = filename.slice(9);
+    newLook.image = req.body.image;
     newLook.email = req.body.email;
     newLook.linkURL = req.body.linkURL;
     newLook.description = req.body.description;
@@ -70,15 +64,12 @@ exports.scrapeUpload = function(req, res) {
           .json(item);
       }
     });
-  });
 }
 
 exports.upload = function(req, res) {
   var newLook = new Look();
-  var fileimage = req.middlewareStorage.fileimage;
-
   console.log(req.body);
-  newLook.image = '/assets/images/uploads/' + fileimage;
+  newLook.image = req.body.image;
   newLook.email = req.body.email;
   newLook.linkURL = req.body.linkURL;
   newLook.title = req.body.title;
